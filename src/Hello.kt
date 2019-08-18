dce5b59551ea3135f2e2d561197976827ee861e4
Alexey <akorchaginn@gmail.com>
2019-08-18 10:54:07 +0300

    Задание 2. Double

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,10 @@
 fun main (args: Array<String>) {
     println("Hello, world)")
+
+
+    var someVar: Any?
+    someVar = 326
+    var doubleVar = someVar / 2.5
+
+    println(doubleVar::class.qualifiedName)
 }
