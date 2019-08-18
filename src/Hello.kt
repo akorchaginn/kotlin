a4fcb77800c68e2462b020be727e4e2d0072d0fe
Alexey <akorchaginn@gmail.com>
2019-08-18 11:19:38 +0300

    Задание 3. Привет Аплана

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,10 +1,17 @@
 fun main (args: Array<String>) {
+//Задание 1
     println("Hello, world)")
 
-
+//Задание 2
     var someVar: Any?
     someVar = 326
     var doubleVar = someVar / 2.5
 
     println(doubleVar::class.qualifiedName)
+
+//Задание 3
+    val hello: String = "Hello"
+    val aplana: String = "Aplana"
+
+    print(hello + " " + aplana + "!")
 }
