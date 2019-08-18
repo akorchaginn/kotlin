2895fb5cb42d6f59ea664f80465b5ecc919c7e6d
Alexey <akorchaginn@gmail.com>
2019-08-18 13:33:49 +0300

    Всё-таки 3 задание. Преобразование типов

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,17 +1,30 @@
 fun main (args: Array<String>) {
-//Задание 1
     println("Hello, world)")
 
-//Задание 2
+//Задание 1
     var someVar: Any?
     someVar = 326
     var doubleVar = someVar / 2.5
 
     println(doubleVar::class.qualifiedName)
 
-//Задание 3
+//Задание 2
     val hello: String = "Hello"
     val aplana: String = "Aplana"
 
-    print(hello + " " + aplana + "!")
+    print("$hello $aplana!")
+
+//Задание 3
+
+    var five: Char = '5'
+
+    var fiveString = five.toString()
+    var fiveByte = fiveString.toByte()
+    var fiveShort = fiveString.toShort()
+    var fiveInt = fiveString.toInt()
+    var fiveDouble = fiveString.toDouble()
+    var fiveFloat = fiveString.toFloat()
+    var fiveLong = fiveString.toLong()
+
+    println("\n" + fiveShort + (fiveByte + fiveLong * fiveInt) / fiveDouble + fiveFloat)
 }
